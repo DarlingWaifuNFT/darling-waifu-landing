9ac740e2416ef15c482e1e5284f2a11cd426124e
DkillG <luisdanielxd06@gmail.com>
2022-04-17 01:51:51 +0100

    sold out change

--- src/components/layouts/callToAction.js
+++ src/components/layouts/callToAction.js
@@ -1,21 +1,19 @@
 import React, {useState, useEffect} from "react";
 import Button from "../elements/button";
 import ButtonOnClick from "../elements/buttonOnClick";
-import Countdown from "../elements/countdown";
 import lock from "../../assets/vector/lock.svg";
 import paper from "../../assets/vector/paper.svg";
 import isologo from "../../assets/vector/isologo.svg";
+import soldOutIcon from "../../assets/img/soldOut.png";
 import { FaWallet } from 'react-icons/fa';
 
 import useClaimPeach from "../../hooks/useClaimPeach";
 
 const CTA = () => {
   const { connectToMetamask, claimeable, claimActive, claimTokens, account } = useClaimPeach()
-  const [counter, setCounter] = useState(true) 
   
   return (
     <div className="absolute lg:relative top-0 left-0 w-full lg:w-1/2 flex flex-col items-center mx-auto my-auto h-full pt-third md:pt-32 lg:pt-32 md:px-8 lg:px-6">
-      {counter >= 0 && <Countdown setCounter={setCounter} />}
       <div className="w-full md:w-3/4 px-20 md:px-0 lg:w-full flex flex-center content-center">
         <img
           className="lg:transform lg:scale-110 w-auto h-auto"
@@ -27,11 +25,12 @@ const CTA = () => {
         Play to Business NFT
       </div>
       <div className="w-full flex flex-col space-y-4 content-center items-center justify-center">
-        {counter <= 0 && <div className="-px-4 flex flex-row space-x-4 content-center items-center justify-center">
-          <Button link="https://www.pinksale.finance/#/launchpad/0x9388bFF3a9Dc5cbFA5960A0ce555e51E2f3213AE" title="Buy Peach Now">
+        <div className="-px-4 flex flex-row space-x-4 content-center items-center justify-center">
+          <div className="absolute rotate-45 w-44 lg:w-56 z-50"><img src={soldOutIcon} alt="" /></div>
+          <Button link="/" title="Buy Peach Now">
               <div>Buy Peach Now</div>
-            </Button>
-          </div>}
+          </Button>
+        </div>
         <div className="-px-4 flex flex-row space-x-4 content-center items-center justify-center">
           <Button
             link="https://beta.darlingwaifu.com"
